fafe05929fdd8e1a5f3539195d893ccbd85a0055
thomasg13 <thomasguo13@gmail.com>
2024-01-15 15:11:36 -0500

    Upload Everything

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2024-01-14T22:40:14.905-0500";
+}
